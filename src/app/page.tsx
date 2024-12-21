aaf3ce076c417f1cf35f41b06f9f6a901f90691f
Your Name <danteocualesjr@gmail.com>
2024-12-21 16:23:24 +0800

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,9 @@ export default function Home() {
             <li><a href="#contact" className="text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white">Contact</a></li>
             
           </ul>
-          
+
         </nav>
+        
       </header>
 
       <main>
