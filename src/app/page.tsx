eb730e92d1262badf9b701a48a6e6b162a196456
Your Name <danteocualesjr@gmail.com>
2024-12-25 16:56:21 +0800

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,9 +52,10 @@ export default function Home() {
                 <Linkedin size={24} />
 
               </a>
+              
               <a href="mailto:danteocualesjr@gmail.com" className="text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white">
                 <Mail size={24} />
-                
+
               </a>
             </div>
             <div className="flex justify-center space-x-4">
