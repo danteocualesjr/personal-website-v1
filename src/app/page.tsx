6c05ca8973486cd39cc89dc64ce3bf5b8044f007
Your Name <danteocualesjr@gmail.com>
2025-01-27 10:09:29 +0800

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -178,8 +178,9 @@ export default function Home() {
                   </div>
                   
                 </div>
-                
+
               </div>
+              
             </div>
           </div>
         </section>
