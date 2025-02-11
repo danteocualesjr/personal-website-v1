c68e348b51ac23de867211e98a844e4d0d8c6f68
Your Name <danteocualesjr@gmail.com>
2025-02-11 18:39:31 +0800

    refactored code

--- src/app/bouncing_ball.py
+++ src/app/bouncing_ball.py
@@ -36,6 +36,7 @@ hex_rotation = 0.0  # current rotation angle in radians
 hex_angular_velocity = math.radians(30)  # 30 degrees per second
 
 # Ball properties
+
 ball_radius = 10
 ball_pos = [hex_center[0], hex_center[1] - 100]
 ball_vel = [150.0, 0.0]  # initial velocity
